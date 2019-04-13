11bafc1d9967ce967ab28d482a498c03515e6306
Ushinji <siguma971@gmail.com>
2019-04-13 14:26:01 +0900

    Remove project action except show.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  resources :projects, param: :key_hash
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :projects, param: :key_hash, only: [:show]
 end
